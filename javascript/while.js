c024109dd2c0e8b4916d11b2a09c9c95a25bbf3f
Aline Cristina Viana da Costa <alinecristinaviana@gmail.com>
2022-09-07 17:37:21 -0300

    add files

---
+++ javascript/while.js
@@ -0,0 +1,20 @@
+// Estrutura de repetição
+// While -> é muito usado quando não sabe o momento da parada
+
+// Example 01
+let i = 0;
+
+while(i < 10) {
+    console.log(i);
+
+    i++;
+}
+
+// Example 02
+let i = 49586394505;
+
+while(i > 10) {
+    console.log(i);
+
+    i /= 35;
+}
