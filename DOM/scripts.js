a274e8eca5be69a5d99eabb85e0c15eb66f1b022
Aline Cristina Viana da Costa <alinecristinaviana@gmail.com>
2022-09-19 14:36:33 -0300

    update

--- DOM/scripts.js
+++ DOM/scripts.js
@@ -52,3 +52,60 @@ element.classList.add('active', 'green')
 console.log(element.classList)
 element.classList.remove('active')
 element.classList.toggle('active')
+
+
+// navegando pelos elementos
+// parentNode parentElement
+const body = document.querySelector('body')
+console.log(body.parentNode)
+
+
+// childNodes children
+const el = document.querySelector('body')
+console.log(el.childNodes)
+console.log(el.children)
+
+// firstChild firstElementChild
+console.log(el.firstElementChild)
+
+// lastChild lastElementChild
+console.log(el.lastElementChild)
+
+
+// nextSiblings nextElementSibling
+const elem = document.querySelector('header')
+console.log(elem.nextElementSibling)
+
+// previousSibling previousElementSibling
+const elemen = document.querySelector('body script')
+console.log(elemen.previousSibling)
+
+
+// Criando e adicionando elementos
+// createElement
+const div = document.createElement('div')
+div.innerText = 'Olá devs!'
+
+// append prepend
+const bodies = document.querySelector('body')
+
+bodies.append(div) // add depois do script e o prepend add antes 
+
+// insertBefore
+const script = body.querySelector('script')
+bodies.insertBefore(div, script)
+
+// Eventos
+const h1 = document.querySelector('h1');
+
+h1.addEventListener('mouseover', print)
+
+function print() {
+    console.log('print')
+}
+
+const input = document.querySelector('input')
+
+input.onkeydown = function() {
+    console.log('rodei')
+}
