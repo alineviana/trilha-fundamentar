18229ebb685eb12d685f40b9bffba9c100d2d1ea
Aline Cristina Viana da Costa <alinecristinaviana@gmail.com>
2022-09-23 18:01:21 -0300

    add files

---
+++ nodejs/events-node/events.js
@@ -0,0 +1,21 @@
+/*
+
+Event Module
+
+É um mecanismo para:
+- Disparar eventos
+- Ouvir eventos
+- Fazer alguma ação quando ouvir o evento
+- É a base para mutos outros módulos como: http, stream, file system...
+
+*/
+
+const { EventEmitter} = require('events');
+const ev = new EventEmitter();
+
+ev.on('saySomething', (message) => {                 // on -> sempre // once -> uma vez
+    console.log('I heard you', message)
+})
+
+ev.emit('saySomething', 'Aline')
+ev.emit('saySomething', 'Mayk')
